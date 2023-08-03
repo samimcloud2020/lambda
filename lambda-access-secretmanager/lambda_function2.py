8b367401257e63b5663e79595ee9c1e8e09c5a31
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-08-03 22:12:51 +0530

    Create lambda_function2.py

---
+++ lambda-access-secretmanager/lambda_function2.py
@@ -0,0 +1,112 @@
+import json
+import boto3
+import base64
+from botocore.exceptions import ClientError
+import pymysql
+from mysql.connector import Error as mysql_error
+import os
+import logging
+
+# Use this code snippet in your app.
+# If you need more information about configurations or implementing the sample code, visit the AWS docs:   
+# https://aws.amazon.com/developers/getting-started/python/
+def get_secret():
+
+    secret_name = "secret3"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+        return secret  # returns the secret as dictionary
+        
+def lambda_handler(event, context):
+
+    # TODO implement
+    credentials = json.loads(get_secret())
+    print(credentials)
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logging.info('Generate database token...')
+
+    rds_client = boto3.client('rds')
+    database_token = rds_client.generate_db_auth_token(
+        hostname = credentials['host'],
+        dbname = credentials['dbname'],
+        port = int(credentials['port']),
+        username = credentials['username'],
+        password = credentials['password']        
+    )
+
+    logging.info('Token successfully obtained. Connecting to database...')
+
+
+  
+    hostname = credentials['host']
+    dbname = credentials['dbname']
+    port = int(credentials['port'])
+    username = credentials['username']
+    password = credentials['password']
+
+    conn = pymysql.connect(
+        host=hostname,
+        database=dbname,
+        port=port,
+        user=username,
+        password=database_token
+        )
+
+    cur = conn.cursor()
+    cur.execute("select * from products")
+    output = cur.fetchall()
+    print(output)
+      
+    # To close the connection
+    conn.close()
+    
+# Driver Code
+if __name__ == "__main__" :
+    mysqlconnect()   
+    
+        
